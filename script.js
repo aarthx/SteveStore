604c56a5b264978177ef992e4d28be2c272dcccc
aarthx <arthuraugusto.exe@gmail.com>
2024-05-19 15:50:18 -0300

    Adicionei as paginas de login e registrar e arrumei ordem de load do conteudo

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
+// Carrega arquivos injetando-os no html no site
 const site = document.getElementById('root')
-carregarComponente('./components/header.html', site)
-carregarComponente('./components/banner.html', site)
-carregarComponente('./components/home.html', site)
-carregarComponente('./components/footer.html', site)
+document.addEventListener('DOMContentLoaded', () => {
+    const URLComponentesIniciais = ['./components/header.html', './components/home.html', './components/footer.html']
+    carregarComponente(URLComponentesIniciais, site)
+})
 
-function carregarComponente(urlComponent, componentePai) {
-    fetch(urlComponent)
-        .then(response => response.text())
-        .then(html => {
-            site.innerHTML += html
+async function carregarComponente(URLComponentesIniciais, componentePai) {
+    try {
+        for(const url of URLComponentesIniciais) {
+            const response = await fetch(url)
+            const texto = await response.text()
+            componentePai.innerHTML += texto
+        }
+        configurarEventos();
+    } catch (e) {
+        return console.error(e)
+    }
+}
+
+function configurarEventos() {
+    //Seleciona a div do conteudo principal para colocar a tela de login e de registrar ao clicar nos respectivos botões
+    
+    const mainContent = document.getElementById('conteudoPrincipal')
+    const botaoEntrar = document.getElementById('btnEntrar')
+    const botaoRegistrar = document.getElementById('btnRegistrar')
+    const URLformularios = ['./components/loginComponent.html', './components/registerComponent.html']
+
+    botaoEntrar.addEventListener('click', () => {
+        fetch(URLformularios[0]).then(response => response.text()).then(html => 
+            mainContent.innerHTML = html
+        )
     })
-        .catch(e => console.error(e))
+
+    botaoRegistrar.addEventListener('click', () => {
+        fetch(URLformularios[1]).then(response => response.text()).then(html => 
+            mainContent.innerHTML = html
+        )
+    })
 }
